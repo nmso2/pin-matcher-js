f2637442a1df846ee8ed07beef4c539d2728eda1
Md. Nagib Mahfuz Subho <nmsubho@gmail.com>
2021-08-18 20:25:47 +0600

    button disabled

--- app.js
+++ app.js
@@ -32,3 +32,40 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
         calcInput.value = newNumber;
     }
 });
+
+// submit button event 
+
+document.getElementById("submit-btn").addEventListener('click', function () {
+    const pin = document.getElementById('display-pin').value;
+    console.log(pin)
+    const typedNumbers = parseInt(document.getElementById('typed-numbers').value);
+    console.log(typedNumbers)
+
+    const successMassege = document.getElementById("notify-success");
+    const errorMassege = document.getElementById("notify-fail");
+    if (pin == typedNumbers) {
+        successMassege.style.display = 'block';
+        errorMassege.style.display = 'none';
+    } else {
+        successMassege.style.display = 'none';
+        errorMassege.style.display = 'block';
+        let actionLeft = parseInt(document.getElementById('action-left').innerText);
+        console.log(actionLeft);
+        actionLeft--;
+        console.log(actionLeft);
+        document.getElementById('action-left').innerText = actionLeft;
+        console.log(actionLeft);
+        if (actionLeft==0) {
+            document.getElementById('generate-pin').setAttribute("disabled",true);
+            document.getElementById('generate-pin').style.background = 'gray';
+            document.getElementById("submit-btn").setAttribute("disabled",true);
+            document.getElementById("submit-btn").style.background = 'gray';
+            const buttons = document.getElementsByClassName('button');
+            for (const button of buttons) {
+                button.setAttribute("disabled",true);
+                button.style.background = 'gray';
+            }
+            
+        }
+    }
+});
